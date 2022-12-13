915d5183ffb18f8bbc483f0f30ee84d20279d09d
Natalie Jones <devnat212@gmail.com>
2022-12-12 23:36:58 -0500

    Made code prettier

--- 11-22.py
+++ 11-22.py
@@ -5,10 +5,10 @@ def process(file):
     file_lines = []
     monkey = 0
     collection = []
-    operation = ""
-    test = 0
-    true_throw = 0
-    false_throw = 0
+    operations = []
+    tests = []
+    true_throws = []
+    false_throws = []
     monkey_count = [0] * 10
     rmonkey = "Monkey (\d+)"
     rstart = "Starting items:(.+)"
@@ -23,37 +23,35 @@ def process(file):
     f = open(file,'r')
     for line in f:
         file_lines.append(line)
-
-    
-    for round in range(20):
         
-        for line in file_lines:
-            if re.search(rmonkey,line):
-                monkey = int(re.search(rmonkey,line).group(1))
-            elif re.search(rstart,line):
-                if round == 0:
-                    exec("collection[monkey] = collection[monkey] + [" + re.search(rstart,line).group(1) + "]")
-            elif re.search(roperation,line):
-                operation = re.search(roperation,line).group(1)
-            elif re.search(rtest,line):
-                test = int(re.search(rtest,line).group(1))
-            elif re.search(rtrue, line):
-                true_throw = int(re.search(rtrue,line).group(1))
-            elif re.search(rfalse,line):
-                false_throw = int(re.search(rfalse,line).group(1))
-            elif len(line) <= 2:
-                for s in collection[monkey].copy():
-                    monkey_count[monkey] += 1
-                    ldic = {'old' : s}
-                    exec(operation,ldic)
-                    new = ldic['new']
-                    worry = new // 3
-                    if worry % test == 0:
-                        collection[true_throw].append(worry)
-                        collection[monkey].remove(s)
-                    else:
-                        collection[false_throw].append(worry)
-                        collection[monkey].remove(s)
+    for line in file_lines:
+        if re.search(rmonkey,line):
+            monkey = int(re.search(rmonkey,line).group(1))
+        elif re.search(rstart,line):
+            exec("collection[monkey] = collection[monkey] + [" + re.search(rstart,line).group(1) + "]")
+        elif re.search(roperation,line):
+            operations.append( re.search(roperation,line).group(1))
+        elif re.search(rtest,line):
+            tests.append( int(re.search(rtest,line).group(1)))
+        elif re.search(rtrue, line):
+            true_throws.append(int(re.search(rtrue,line).group(1)))
+        elif re.search(rfalse,line):
+            false_throws.append(int(re.search(rfalse,line).group(1)))
+
+    for round in range(20):
+        for monkey in range(0,len(operations)):
+            for s in collection[monkey].copy():
+                monkey_count[monkey] += 1
+                ldic = {'old' : s}
+                exec(operations[monkey],ldic)
+                new = ldic['new']
+                worry = new // 3
+                if worry % tests[monkey] == 0:
+                    collection[true_throws[monkey]].append(worry)
+                    collection[monkey].remove(s)
+                else:
+                    collection[false_throws[monkey]].append(worry)
+                    collection[monkey].remove(s)
     return monkey_count
                     
 
